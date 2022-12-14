d34d83795211718ea0c5c38b315a78b838a78674
Allan GONIN-SAGET <allan.gonin-saget.etu@univ-lemans.fr>
2022-12-14 15:38:16 +0100

    connexion

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.login('Nzg4MDYxNDY1MjAyNTI0MTYx.Gl6w4n.0ty5hN3fEM4j1z7jjjCMVc-MrgE_FhGnGfQR44');
+
+
