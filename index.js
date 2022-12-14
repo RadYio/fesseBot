085d6778dc3da1a9b8c67aeda028f7c34ecba136
Allan GONIN-SAGET <allan.gonin-saget.etu@univ-lemans.fr>
2022-12-15 00:39:38 +0100

    ajout debug et enfin reponse au @

--- index.js
+++ index.js
@@ -2,33 +2,41 @@
 const Discord = require('discord.js');
 
 //création d'un nouveau client discord
-const client = new Discord.Client({ intents: 1 })
+const client = new Discord.Client({ intents: 131027 });
 
+const debug = false;
 //Récupération du token dans le fichier
 const fs = require('fs')
 const token = fs.readFileSync('token.token', 'utf8')
 console.log(`Token trouvé:  ${token}!`);
 
+
+const id_salon_test = '280773885287333888';
 //quand le client est prêt:
 client.on('ready', () => {
     console.log(`Connecté en tant que ${client.user.tag}!`);
-    const channel = client.channels.cache.get('892842167226208328');
-    channel.send('Je suis connecté!');
+    const channel = client.channels.cache.get(id_salon_test);
+    channel.send('Hello!!');
 });
 
 
 //quand le client reçoit un message:
-client.on('message', msg => {
-  console.log(`je recois un message: ${msg.content}!\n`);
-  
-  if (msg.content === 'fesse') {
-    msg.reply('fesse');
+client.on('messageCreate', msg => {
+  if( debug ) console.log(`Message de {${msg.author.username}}: ${msg.content}`);
+  if(msg.mentions.users.first() === client.user){
+    texte = msg.content.substring(client.user.id.length+4)
+    
+    if( debug ) console.log(texte);
+
+    if(texte.toLowerCase() === 'bonjour'){
+      msg.reply('Bonjour!');
+    }
   }
 });
 
 client.on('disconnect', message => {
-  const channel = client.channels.cache.get('892842167226208328');
-  channel.send('Je suis pars!');
+  const channel = client.channels.cache.get(id_salon_test);
+  channel.send('Je pars avec toutes les fesses!');
 });
 
 
