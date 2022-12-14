33e11e1ba491f75b806c0690bc16e668f7796639
Allan GONIN-SAGET <allan.gonin-saget.etu@univ-lemans.fr>
2022-12-15 00:42:42 +0100

    commentaire et dodo pour moi pas pour le bot

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const Discord = require('discord.js');
 //création d'un nouveau client discord
 const client = new Discord.Client({ intents: 131027 });
 
+//On active le mode debug
 const debug = false;
+
 //Récupération du token dans le fichier
 const fs = require('fs')
 const token = fs.readFileSync('token.token', 'utf8')
 console.log(`Token trouvé:  ${token}!`);
 
+//id du salon qui recevra les messages de test
+const id_salon_test = '280773885287333888'; //bsl 
 
-const id_salon_test = '280773885287333888';
+
 //quand le client est prêt:
 client.on('ready', () => {
     console.log(`Connecté en tant que ${client.user.tag}!`);
@@ -23,11 +27,15 @@ client.on('ready', () => {
 //quand le client reçoit un message:
 client.on('messageCreate', msg => {
   if( debug ) console.log(`Message de {${msg.author.username}}: ${msg.content}`);
+  
+  //On verifie que le message est destiné au bot
   if(msg.mentions.users.first() === client.user){
+    //On transforme en le message pour ne garder que le contenu sans la mention
     texte = msg.content.substring(client.user.id.length+4)
-    
+
     if( debug ) console.log(texte);
 
+    //On fait les tests sur le messages
     if(texte.toLowerCase() === 'bonjour'){
       msg.reply('Bonjour!');
     }
