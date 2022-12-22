c7612a9421f2c94295d0a04f1675e8d78b8f4c1a
Allan GONIN-SAGET <allan.gonin-saget.etu@univ-lemans.fr>
2022-12-22 18:57:37 +0100

    ajout de pas mal de lib et une commande /vocal

---
+++ commandes/vocal.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder, PermissionFlagsBits, ChannelType } = require('discord.js');
+const { joinVoiceChannel } = require('@discordjs/voice');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('vocal')
+		.setDescription('[beta] crée un salon vocal et le bot rejoint ce salon'),
+	async execute(interaction) {
+        await interaction.deferReply({ephemeral: true});
+        const nom_salon = interaction.user.username + '_fesse';
+		const salon = await interaction.guild.channels.create({
+            name: nom_salon,
+            type: ChannelType.GuildVoice,
+            permissionOverwrites: [
+                {
+                id: interaction.user.id,
+                deny: [PermissionFlagsBits.ViewChannel],
+                },
+            ],
+        });
+        
+        await interaction.editReply({content: 'Salon vocal créé', ephemeral: true});
+        const connection = joinVoiceChannel({
+            channelId: interaction.guild.channels.cache.find(channel => channel.name === nom_salon).id,
+            guildId: interaction.guild.id,
+            adapterCreator: interaction.guild.voiceAdapterCreator,
+        });
+
+        setTimeout(() => {interaction.deleteReply()}, 5000);
+        
+
+	},
+};
+
+
