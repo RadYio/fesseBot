787ee7ff1125b55f2fc8734bb07bf26065d07a34
Allan GONIN-SAGET <allan.gonin-saget.etu@univ-lemans.fr>
2022-12-22 15:04:09 +0100

    commande ping qui se delete automatiquement

--- commandes/ping.js
+++ commandes/ping.js
@@ -5,9 +5,12 @@ module.exports = {
 		.setName('ping')
 		.setDescription('donne son temps de réponse en ms'),
 	async execute(interaction) {
-		await interaction.reply({content: 'je regarde..', ephemeral: true}).then(async () => {
-			await interaction.editReply( {content: (interaction.client.ws.ping) + "ms", ephemeral: true});
-		});
+		//On pourrait utiliser deferReply, mais le bot est sensé repondre instantanément donc on garde reply
+		//await interaction.deferReply({ephemeral: true});
+		await interaction.reply({content: 'Je regarde...', ephemeral: true});
+		await interaction.editReply( {content: (interaction.client.ws.ping) + "ms", ephemeral: true});
+		await setTimeout(() => {interaction.deleteReply()}, 5000);
+		
 	},
 };
 
