fb57c2bd9f062339a03ab676767c47cb38e99084
Allan GONIN-SAGET <allan.gonin-saget.etu@univ-lemans.fr>
2022-12-24 22:39:30 +0100

    ajout de dépendance pour la gestion de bdd

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,22 +3,24 @@ const { Events } = require('discord.js');
 module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
-		if (!interaction.isChatInputCommand()) return;
-
-        //On recupere le nom de la commande 
-        const commande = interaction.client.commandes.get(interaction.commandName);
-        //verification que la commande existe
-	    if (!commande) {
-		    console.error(`No command matching ${interaction.commandName} was found.`);
-		    return;
-	    }
-        //On execute la commande
-	    try {
-		    await commande.execute(interaction);
-	    } catch (error) {
-		    console.error(error);
-		    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-			setTimeout(() => {interaction.deleteReply()}, 30000);
-	    }
+		if (interaction.isChatInputCommand()){
+			//On recupere le nom de la commande 
+			const commande = interaction.client.commandes.get(interaction.commandName);
+			//verification que la commande existe
+			if (!commande) {
+				console.error(`No command matching ${interaction.commandName} was found.`);
+				return;
+			}
+			//On execute la commande
+			try {
+				await commande.execute(interaction);
+			} catch (error) {
+				console.error(error);
+				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+				setTimeout(() => {interaction.deleteReply()}, 30000);
+			}
+			return;
+		}
+			
 	},
 };
