e05d226111e23b328f2130cfcd182905192ddc2a
Allan GONIN-SAGET <allan.gonin-saget.etu@univ-lemans.fr>
2022-12-25 01:14:12 +0100

    test du spoiler toujours pas concluant

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -11,10 +11,10 @@ module.exports = {
         //Le but originel de ce bot
         if (interaction.content.toLocaleLowerCase().includes('fesse')) {
             if (interaction.author.id === ownerId){
-                interaction.reply('https://instagram.frns1-1.fna.fbcdn.net/v/t51.2885-15/300599829_1708577599520104_4659010997660154779_n.jpg?stp=dst-jpg_e35&_nc_ht=instagram.frns1-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=dD-qu7FDnE0AX8FDd6R&edm=ABmJApABAAAA&ccb=7-5&ig_cache_key=MjkxMTI3MzM2MDAzODY1OTg3NQ%3D%3D.2-ccb7-5&oh=00_AfBmcwaEt3G4uJqZFSnaZLA1NOCBcPnbmgpDY5B-zf7bZA&oe=63A02F54&_nc_sid=6136e7');
+                interaction.reply({content: 'https://media2.giphy.com/media/6D0dubMvJUtAA/giphy.gif?cid=790b761164b740deaf6eeb7b9de91d5e426b5c582d244c26&rid=giphy.gif&ct=g', spoiler: true});
             }else if(interaction.author.id === '281571816848228352'){ //alexis
                 interaction.react('🏳️‍🌈');
-                interaction.reply('||test||', {files: "./image/alexispic.png"});
+                interaction.reply({files: "./image/alexispic.png", spoiler: true});
             }else{
                 interaction.react(interaction.guild.emojis.cache.find(emoji => emoji.name === 'gronin'));
             }
